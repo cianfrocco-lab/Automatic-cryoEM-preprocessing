a6f8ecfce9524ba0c0bcbfcd773a57ac570c9f34
yilaili <yilai@umich.edu>
2021-05-31 19:16:18 -0400

    sync

--- cryoassess/micassess.py
+++ cryoassess/micassess.py
@@ -208,7 +208,7 @@ def loop_files(labels, args):
         if i < 2:
             goodlist = list(sorted(glob.glob(os.path.join(test_data_dir, 'data', '*.png'))) for ii in idx)
         for j in idx:
-            file = sorted(glob.glob(os.path.join(test_data_dir, 'data', '*.png')))[j]
+            file = sorted(glob.glob(os.path.join(test_data_dir, 'data', '*.png')))[int(j)]
             shutil.copy2(file, os.path.join(args['output'], label_list[i]))
 
     shutil.rmtree(test_data_dir)
